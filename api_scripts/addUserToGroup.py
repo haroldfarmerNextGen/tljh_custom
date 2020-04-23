083eedfe26e48109c9b9a3301429aeffb9d2f5bd
Harold <hfarmer@nextgenfed.com>
2020-04-23 14:54:03 -0400

    nothing changed

--- api_scripts/addUserToGroup.py
+++ api_scripts/addUserToGroup.py
@@ -3,8 +3,8 @@ import requests
 api_url = 'http://localhost:12000/hub/api'
 token = '5921bc46c0e24400b8e35882a239f725'
 
-data = {'name': 'dev', 'users': ['jimmy','john']}
-r = requests.post(api_url + '/groups/dev/users',
+data = {'name': 'user', 'users': ['test4']}
+r = requests.post(api_url + '/groups/user/users',
     headers={
              'Authorization': 'token %s' % token,
             },
